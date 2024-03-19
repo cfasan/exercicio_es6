6c375c124282c55d531134d82bcc1a38fde0284c
Claudio Favoreto <claudio.favsan@gmail.com>
2024-03-18 21:44:08 -0300

    First commit

---
+++ dist/medias.js
@@ -0,0 +1,21 @@
+const media = 6;
+const mediaAlunos = [{
+  nome: 'Braulênio',
+  nota: 9
+}, {
+  nome: 'Astolfo',
+  nota: 5
+}, {
+  nome: 'Astrirdes',
+  nota: 10
+}, {
+  nome: 'Albanens',
+  nota: 4
+}, {
+  nome: 'Domênico',
+  nota: 10
+}];
+const aprovados = mediaAlunos.filter(item => {
+  return item.nota >= media;
+});
+console.log(aprovados);
